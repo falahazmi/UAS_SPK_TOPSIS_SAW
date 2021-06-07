c50a0aea6a14341285a4798e6d5ce5a1dfad3da7
FalaahAzmi <54742143+falahazmi@users.noreply.github.com>
2021-06-08 01:12:12 +0700

    Add files via upload
    Last Commit

---
+++ template-parts/footer.php
@@ -0,0 +1,18 @@
+	
+	</div><!-- #main -->
+	
+	<footer id="footer">
+		<div class="container">
+			<p>Kelompok 3</a></p>
+		</div>
+	</footer>
+
+	</div><!-- #page -->
+</body>
+</html>
+<?php
+if(isset($pdo)) {
+	// Tutup Koneksi
+	$pdo = null;
+}
+?>
